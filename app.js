254a98c04fea5d3dc01b9d789390e2d476133e77
Luis Carrazco <luiscarrazco@gmail.com>
2019-05-30 18:10:04 -0600

    Game function rules

---
+++ app.js
@@ -0,0 +1,9 @@
+/*
+GAME FUNCTION:
+- Player must guess a number between a min and max
+- Plaher gets a certain amount of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if lost
+- Let player choose to play again
+*/
+
